ffc61f69ec0dc209a8f7d8e7a5977e9233ac9a7a
joshayres <me@joshayres.tech>
2021-10-28 14:45:57 -0700

    set up product routes

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  auth = 'PUT_TOKEN_HERE';
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax',
+  token: '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'
 };
--- server/router/products.js
+++ server/router/products.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const config = require('../config.js');
 
 router.get('/', (req, res) => {
-  axios.get('')
+  axios.get(`${config.url}/products`, {
+    headers: {
+      'Authorization': config.token
+    }
+  })
+    .then(data => {
+      res.send(data.data).status(200);
+    })
+    .catch(err => res.send(err).status(500));
 });
+
+router.get('/:product_id', (req, res) => {
+  axios.get(`${config.url}/products/${req.params.product_id}`, {
+    headers: {
+      'Authorization': config.token
+    }
+  })
+    .then(data => {
+      res.send(data.data).status(200);
+    })
+    .catch(err => res.send(err).status(500));
+});
+
+router.get('/:product_id/styles', (req, res) => {
+  axios.get(`${config.url}/products/${req.params.product_id}/styles`, {
+    headers: {
+      'Authorization': config.token
+    }
+  })
+    .then(data => {
+      res.send(data.data).status(200);
+    })
+    .catch(err => res.send(err).status(500));
+});
+
+router.get('/:product_id/related', (req, res) => {
+  axios.get(`${config.url}/products/${req.params.product_id}/related`, {
+    headers: {
+      'Authorization': config.token
+    }
+  })
+    .then(data => {
+      res.send(data.data).status(200);
+    })
+    .catch(err => res.send(err).status(500));
+});
+
+
 
 module.exports = router;
