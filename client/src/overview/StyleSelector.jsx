2884f8caeb7a51128c216e8247c650340264819a
joshayres <me@joshayres.tech>
2021-11-08 11:47:05 -0800

    check mark

--- client/src/overview/StyleSelector.jsx
+++ client/src/overview/StyleSelector.jsx
@@ -4,15 +4,42 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 let Style = (props) => {
+  if (!props.selected) {
+    return (
+      <div>
+        <img
+          className='d-inline w-75 py-3'
+          src={props.data.photos[0].thumbnail_url}
+          onClick={() => {
+            props.selectStyle(props.index);
+          }}
+        />
+      </div>
+    )
+  }
   return (
     <div>
       <img
-        className='d-inline w-50'
+        className='d-inline w-75 py-3'
         src={props.data.photos[0].thumbnail_url}
         onClick={() => {
           props.selectStyle(props.index);
         }}
-        />
+      />
+      <img
+        style={
+          {
+            position: 'absolute',
+            transform: 'translate(-50%, 0%)',
+            width: '50px',
+            height: '50px',
+            backgroundColor: 'white',
+            borderRadius: '25px',
+            border: '2px solid black'
+          }
+        }
+        src='https://www.freeiconspng.com/thumbs/checkmark-png/black-checkmark-png-4.png'
+      />
     </div>
   )
 }
@@ -27,7 +54,7 @@ let StyleSelector = (props) => {
           {props.data.results.map((item, index) => {
             return (
               <Col>
-                <Style data={item} selectStyle={props.selectStyle} index={index} />
+                <Style data={item} selectStyle={props.selectStyle} index={index} selected={index === props.selectedStyle} />
               </Col>
             )
           })}
